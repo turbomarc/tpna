becdffc1abf20280b568d70a12f630c0c316a928
Marc Phillips <marc@thinkrelevance.com>
2013-09-22 23:11:53 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpna::Application.config.secret_token = '8ac8a8dc227240c0a0463e0f4520b4ad8ce1efa265a3d322c2a044f370c0264d0495c408a9db2f4fc294138067a8488bb43bd4fe81f606fc2fb862a116470a1e'
