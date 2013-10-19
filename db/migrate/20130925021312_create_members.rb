d96e61d8402acc1bc765c1a01baa43b22573ed09
Marc Phillips <marc@thinkrelevance.com>
2013-10-19 15:06:50 -0700

    Handle simpler membership sheet

--- db/migrate/20130925021312_create_members.rb
+++ db/migrate/20130925021312_create_members.rb
@@ -3,42 +3,15 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :name
       t.string :allhousenum
-      t.string :status
-      t.integer :join
+      t.date :join
       t.date :updated
       t.date :last_payment
       t.integer :amt
-      t.integer :paid_thru
+      t.date :paid_thru
       t.string :email
       t.string :phone
-      t.string :street_name
       t.string :citystzip
-      t.string :notes
-      t.string :auto_status
-      t.string :street
-      t.string :aptno
-      t.string :moved
-      t.string :st
-      t.decimal :hsenum
-      t.integer :blknum
-      t.string :pos
-      t.string :who
-      t.string :disc
-      t.date :transdate
-      t.string :use
-      t.string :site_misc
-      t.integer :sec
-      t.integer :copy
-      t.integer :blk
-      t.string :side
-      t.string :multi
-      t.integer :evenodd
-      t.string :on
-      t.string :ref
-      t.string :plus
-      t.integer :parcel_id
-      t.string :own_rent
-
+ 
       t.timestamps
     end
   end
