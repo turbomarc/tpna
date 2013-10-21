d9e4cc9771fc486d19fcb4b3923635e0c41beb28
Marc Phillips <marc@thinkrelevance.com>
2013-10-20 18:09:59 -0700

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USER'], :password => ENV['BASIC_AUTH_PASSWORD']
+
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
