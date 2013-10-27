8b80b364cea130cc8922224406240b8b1f53c9ba
Jamie Kite <jamie@thinkrelevance.com>
2013-10-27 14:38:15 -0400

    Add GMaps link, email link, currency formatting.

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,7 @@
 module MembersHelper
+  def link_to_google_maps(address)
+    street_address = address.partition(',')[0]
+    query = u("#{street_address}, Durham, NC")
+    link_to address, "http://maps.google.com/?q=#{query}", target: "_blank"
+  end
 end
