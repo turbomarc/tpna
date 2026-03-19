fce2b297ddcc53fbf2b6f0eae6c3daa1abec59b1
Patrick Wright <prwright@gmail.com>
2026-03-19 10:59:54 -0400

    remove basic auth - we have authentication through login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  unless Rails.env.test? || Rails.env.development?
-    http_basic_authenticate_with :name => ENV['BASIC_AUTH_USER'], :password => ENV['BASIC_AUTH_PASSWORD']
-  end
-
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
